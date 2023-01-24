f127ea598ca420e919687cc66dd9bd69f3de782d
Ederson Duarte Schaukoski <schaukoskientertainment@gmail.com>
2023-01-24 16:49:57 -0300

    Ajuste meta scale

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ function Home({player}) {
     return (
         <>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
             </Head>
             <Jogo />
         </>
